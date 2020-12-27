456bad071e7783674df8018a7b3829f974adf47b
zzc <zhouzhichao-cn@163.com>
2020-12-27 20:07:34 +0800

    04

---
+++ Week_04/kmp.js
@@ -0,0 +1,51 @@
+function kmp(source, pattern) {
+    // 计算table
+    let table = new Array(pattern.length);
+    {
+        let i = 1, j = 0; // j 已重复的
+
+        while (i < pattern.length) {
+            if (pattern[i] === pattern[j]) {
+                ++i, ++j;
+                table[i] = j;
+            } else {
+                if (j > 0) {
+                    j = table[j];
+                } else {
+                    // table[i] = j;
+                    ++i;
+                }
+            }
+
+            
+        }
+
+    }
+
+
+    {
+        let i = 0, j =0;
+        while (i < source.length) {
+            if (pattern[j] === source[i]) {
+                ++i , ++j;
+            }else {
+                if (j > 0) {
+                    j =table[j]
+                }else {
+                    ++i;
+                }
+            }
+            if (j === pattern.length) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+    // console.log(table);
+
+
+    // 匹配
+}
+
+console.log(kmp("abcdeaafghjuy", "abcdeaafghjuy"));
